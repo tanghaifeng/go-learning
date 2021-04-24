cb8b143acb6988241b8ca2ed305c4e350feaec92
tanghaifeng <tanghaifeng@gmail.com>
2021-04-24 23:46:12 +0800

    切片

--- collction/slicedemo.go
+++ collction/slicedemo.go
@@ -4,10 +4,33 @@ import (
 	"fmt"
 )
 
-func SliceDemo() {
-	var arr = []int{1, 2, 3, 4, 5, 6}
-	var arr1 = []int{1, 2, 3, 4, 5, 5}
-	arr = append(append(arr, arr1...), 99)
-	fmt.Println(arr)
+/**
+slice是基于数组，数据结构有三个字段 指向数组的指针，存放的长度，容量，可以动态扩容
 
+*/
+func SliceDemo() {
+	// slice make 声明
+	slice1 := make([]int, 2) // 利用内置函数make来声明函数，make函数声明了切片并且进行了初始化赋值 赋类型的0值
+	fmt.Println(slice1)
+	slice2 := make([]int, 2, 5)
+	fmt.Println(slice2)
+
+	// 字面量声明
+	slice3 := []int{1, 23, 4, 6}
+	fmt.Println(slice3)
+
+	var arr = []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(arr)
+	// 长度 arr[i:j] j-i 容量 k-i
+	arr2 := arr[1:2] // 下表为1的 第二个且不包括第三个的数 [1:2)
+	fmt.Println(arr2, len(arr2), cap(arr2))
+
+	// 迭代
+	for k, v := range arr {
+		fmt.Println(k, v)
+	}
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
 }
