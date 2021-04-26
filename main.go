714b107be8f88657e678afbe18740c11fe5c99a1
TIM <tanghaifeng93@gmail.com>
2021-04-26 10:27:39 +0800

    fmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"go-learning/base"
 	_ "go-learning/base"
+	channel_01 "go-learning/channel-01"
 )
 
 type chan1 struct {
@@ -11,8 +11,9 @@ type chan1 struct {
 func main() {
 	//const a int = 1
 	//fmt.Printf("%T\n", a)
-	base.SetMember()
-	m := base.Member{}
-	m.SetMember()
+	//base.SetMember()
+	//m := base.Member{}
+	//m.SetMember()
+	channel_01.RunChannel1()
 
 }
